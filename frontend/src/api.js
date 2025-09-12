daddaf9e836e7a8a451eb75584cf1ca44f72c5b3
Poorvi Parashar <poorvi.parashar_cs.aiml23@gla.ac.in>
2025-09-12 17:08:35 +0530

    Poorvi Parashar: updated frontend\src\api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
-// Step 5 work by Poorvi
+// Step 3 work by Poorvi
 import axios from 'axios'
 const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8000'
 
 export const predictScratch = (text) => axios.post(`${API_BASE}/predict/scratch`, { text })
 export const predictHF = (text) => axios.post(`${API_BASE}/predict/hf`, { text })
 export const trainScratch = () => axios.post(`${API_BASE}/train`)
-// Step 5: finalized APIs by Poorvi
